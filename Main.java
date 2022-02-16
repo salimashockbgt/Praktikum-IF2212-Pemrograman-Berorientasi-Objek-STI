b2614dca09074ab1f031e9ed42758336d670ce67
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2022-02-16 10:22:25 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,11 @@
 //package com.tutorial;
 //import java.util.Scanner;
-/*
-public class Main {
-    /*public static void main(String[] args){
-        Mahasiswa mahasiswa1 = new Mahasiswa("Ima", 18220054);
-        mahasiswa1.show();
-        mahasiswa1.setOrg2(16520025, "Soli");
-        System.out.println(mahasiswa1.Nama);
-        System.out.println(mahasiswa1.NIM);
-        /*
-        Scanner userInput = new Scanner(System.in);
-        int panjang = userInput.nextInt();
-        System.out.println("panjang = " + panjang);
-        //System.out.println("hello world");
-        int lebar = userInput.nextInt();
-        System.out.println("lebar = " + lebar);
-        int luas = lebar * panjang;
-        System.out.println("Luas = " + luas);
-        userInput.close();
-    }
-}
+
 */
 /**
  * Main.java
  * [jelaskan kegunaan file ini]
- * @author [NIM] [Nama]
+ * @author [18220054] [Salimatussholati Az Zahra]
  */
 import java.lang.System;
 import java.util.Scanner;
